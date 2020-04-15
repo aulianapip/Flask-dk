c37e0728e95895345135ab2f3be35638e8c26b13
aulianapip <naufalapip45@gmail.com>
2020-04-15 16:24:22 +0700

    coba ubah dbmodel

--- app/modul/dbmodel.py
+++ app/modul/dbmodel.py
@@ -20,7 +20,7 @@ class DBModel:
 	def insert_cleaning_data(self, database, collection, documents):
 		db = self.client[database]
 		db[collection].drop()
-		results = db[collection].insert_many(documents.to_dict('records'))
+		results = db[collection].insert_many(str(documents).to_dict('records'))
 		return results.inserted_ids
 
 	def insert_header(self, database, collection, documents):
